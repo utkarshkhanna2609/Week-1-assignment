7aba82703938c2d59733fc1dac9f47aa44a6d4e3
Utkarsh Khanna <107148451+utkarshkhanna2609@users.noreply.github.com>
2023-06-14 08:16:58 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,27 @@
 */
 
 function isAnagram(str1, str2) {
+  const cleanStr2=str2.replace(/[^A-Za-z]/g, "").toLowerCase();
+  const cleanStr1=str1.replace(/[^A-Za-z]/g, "").toLowerCase();
 
-}
+  if(cleanStr1.length!=cleanStr2.length) return false;
+
+  const freqMp1={};
+  const freqMp2={};
+
+  for(let char of cleanStr1){
+    freqMp1[char]=(freqMp1[char] || 0)+1;
+  }
+
+  for(let char of cleanStr2){
+    freqMp2[char]=(freqMp2[char] || 0)+1;
+  }
+
+  for (let char in freqMp1) {
+    if (freqMp1[char] !== freqMp2[char]) {
+      return false;
+    }
+  return true;
+}}
 
 module.exports = isAnagram;
